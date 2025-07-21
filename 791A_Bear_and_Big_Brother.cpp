58187f194a5e4fce3299e219b514bd46147077a4
unknown <mdjame34@gmail.com>
2025-07-21 23:52:53 +0600

    problem

--- 791A_Bear_and_Big_Brother.cpp
+++ 791A_Bear_and_Big_Brother.cpp
@@ -2,7 +2,17 @@
 using namespace std;
 
 int main() {
-   
+    int a,b,year=0;
+    cin >> a>> b;
+ 
+    while(a<=b)
+    {
+        a*=3;
+        a*=
+    }
+    
+ 
+    cout << year << endl;
 
     return 0;
 }
