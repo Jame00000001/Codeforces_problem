8bd1dad0fb4817ee0a564559429d144977b7a6f2
unknown <mdjame34@gmail.com>
2025-07-23 09:47:44 +0600

    update

--- 546A_Soldier_and_Bananas.cpp
+++ 546A_Soldier_and_Bananas.cpp
@@ -6,6 +6,14 @@
 using namespace std;
 
 int main() {
-    
+    int k,n,w,total_amount=0;
+    cin>>k>>n>>w;
+    total_amount = (k*(w*(w+1))/2);
+
+    int borrow = total_amount-n;
+    if(borrow<0)
+       borrow=0;
+    cout << borrow<<endl;
+   
     return 0;
 }
