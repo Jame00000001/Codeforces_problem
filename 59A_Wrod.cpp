50af04da916d26ed7d4b246941c22f0d1484c701
unknown <mdjame34@gmail.com>
2025-07-24 22:02:57 +0600

    fixed problem

--- 59A_Wrod.cpp
+++ 59A_Wrod.cpp
@@ -6,6 +6,34 @@
 using namespace std;
 
 int main() {
+string s;
+cin>> s;
+int small=0,capital=0;
+for(int i = 0 ; i < s.length();i++)
+{
+    if(s[i]>='a'&&s[i]<='z')
+    {
+        small++;
+    }
+    if(s[i]>='A'&&s[i]<='Z')
+    {
+        capital++;
+    }
     
-    return 0;
+}
+// cout<<small << " " <<captial;
+if(small>=capital)
+{
+    for(int i = 0;i<s.length();i++)
+    {
+        s[i]=tolower(s[i]);
+    }
+    cout << s << endl;
+}else{//upercae>lowercase = all letter be upercase
+    for(int i = 0;i<s.length();i++)
+    {
+        s[i]=toupper(s[i]);
+    }
+    cout << s << endl;
+}
 }
